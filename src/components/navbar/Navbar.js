dd1959f5d3fcec8942d0b4af5711e0dc6fb3adad
Daniel Meza <daniel.meza29@yahoo.com>
2020-09-10 22:00:48 -0500

    Simple navbar added

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { withRouter, Link } from 'react-router-dom'
 
-export default function Navbar(props) {
+function Navbar(props) {
     return (
-        <>
-        </>
+        <ul>
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/communities">communities</Link></li>
+            <li><Link to="/newpost">Post</Link></li>
+            <li><Link to="/messages">Messages</Link></li>
+            <li><Link to="/account">Account</Link></li>
+        </ul>
     )
 }
+export default withRouter(Navbar)
