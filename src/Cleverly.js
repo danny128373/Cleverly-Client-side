34096ba4d450f8b694913d3ac60f15a8a1a19b6b
Daniel Meza <daniel.meza29@yahoo.com>
2020-09-10 21:05:59 -0500

    Finished login and register

--- src/Cleverly.js
+++ src/Cleverly.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react'
+import {Route} from 'react-router-dom'
+import ApplicationViews from './ApplicationViews'
+import Navbar from './components/navbar/Navbar'
 
-function App() {
-  return 'test'    
+function Cleverly(props) {
+
+  const [isLogged, setIsLogged] = useState(false)
+
+  return (
+    <>
+      <Route render={props => (
+                <Navbar {...props} isLogged={isLogged} setIsLogged={setIsLogged} />
+            )} />
+      <ApplicationViews isLogged={isLogged} setIsLogged={setIsLogged} {...props}/>
+    </>
+  )    
 }
 
-export default App
+export default Cleverly
+
