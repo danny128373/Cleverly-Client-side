a9a706e0b0ed71d359f1606c85aca7a09d7bb075
Daniel Meza <daniel.meza29@yahoo.com>
2020-09-13 21:19:58 -0500

    Home page now shows all posts from all the communities the user is following

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import ApiManager from '../../api/ApiManager'
+import PostCard from '../posts/PostCard'
 
 export default function Home(props) {
+
+    const [posts, setPosts] = useState([{content:"", likes:"", created_at:"",community:{}, profile:{user:{}}}])
+
+   
+    const getPosts = () => {
+        ApiManager.getPosts().then(posts => setPosts(posts))
+    }
+
+    useEffect(getPosts, [])
+
     return (
         <>
-        This is Home.
+            {posts.map(post => <PostCard {...props} post={post}/>)}
         </>
     )
 }
